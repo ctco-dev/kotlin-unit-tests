7d47aef3090a060020e65d892a551852b9b60cb0
Vitaliy Levashin <Vitaliy_Levashin@rcomext.com>
2019-03-18 10:50:24 +0200

    cases with collections

--- application/src/main/kotlin/com/home/utils/Calculator.kt
+++ application/src/main/kotlin/com/home/utils/Calculator.kt
@@ -1,8 +1,28 @@
 package com.home.utils
 
+import com.home.dto.Data
+import java.util.regex.Pattern
+
 class Calculator {
 
     companion object {
-        fun sum(a: Int, b: String): Int = a + Integer.parseInt(b)
+        fun sum(a: Int, b: String): Int {
+            var bInt = 0
+            try {
+                bInt = Integer.parseInt(b)
+            } catch (e: NumberFormatException) {
+            }
+
+            return a + bInt;
+        }
+
+        //test for normal filtering, filtering of empty data, invalid and empty regex
+        fun filterByContent(data: List<Data>, regex: String?): List<Data> {
+            if (regex == null || regex.isBlank()) {
+                return data
+            }
+            val pattern = Pattern.compile(regex)
+            return data.filter { d -> pattern.matcher(d.content).matches() }
+        }
     }
 }
--- examples-junit/src/test/kotlin/com/home/utils/CalculatorTest.kt
+++ examples-junit/src/test/kotlin/com/home/utils/CalculatorTest.kt
@@ -1,8 +1,14 @@
 package com.home.utils
 
+import com.home.dto.Data
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertThrows
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.*
+import java.util.stream.Stream
 
 internal class CalculatorTest {
 
@@ -12,4 +18,75 @@ internal class CalculatorTest {
     fun shouldSum(a: Int, b: String, result: Int) {
         assertEquals(result, Calculator.sum(a, b))
     }
+
+    data class SumCase(val a: Int, val b: String, val expected: Int)
+
+
+    companion object {
+        @JvmStatic
+        fun sumCaseSource(): Stream<SumCase> {
+            return Stream.of(SumCase(1, "2", 3),
+                    SumCase(-1, "0", -1),
+                    SumCase(-1, "", -1),
+                    SumCase(-1, "abc", -1))
+        }
+    }
+
+    //companion object is required
+    @ParameterizedTest
+    @MethodSource("sumCaseSource")
+    fun shouldSumViaMethodSource(case: SumCase) {
+        assertEquals(case.expected, Calculator.sum(case.a, case.b))
+    }
+
+    //less code, no individual results per case
+    @Test
+    fun shouldSumWithTableCases() {
+        data class Case(val a: Int, val b: String, val expected: Int)
+
+        val cases = listOf(Case(1, "2", 3),
+                Case(-1, "0", -1),
+                Case(-1, "", -1),
+                Case(-1, "abc", -1))
+
+        cases.forEach {
+            assertEquals(it.expected, Calculator.sum(it.a, it.b))
+        }
+    }
+
+    @Test
+    fun shouldFilterByContent() {
+        val data = listOf(Data(1, "abc"), Data(2, "bc"), Data(3, "a"), Data(4, "ab"))
+        val expected = listOf(Data(1, "abc"), Data(4, "ab"))
+        assertEquals(expected, Calculator.filterByContent(data, "a.+"))
+    }
+
+    @Test
+    fun shouldFilterEmpty() {
+        val data = Collections.emptyList<Data>()
+        val expected = Collections.emptyList<Data>()
+        assertEquals(expected, Calculator.filterByContent(data, "a.+"))
+    }
+
+    //ValueSource won't accept null
+
+    //@ParameterizedTest
+    //@ValueSource(strings = arrayOf<String?>( "", null))
+    @Test
+    fun shouldFilterWithoutRegex() {
+        val data = listOf(Data(1, "abc"), Data(4, "ab"))
+        val expected = listOf(Data(1, "abc"), Data(4, "ab"))
+        assertEquals(expected, Calculator.filterByContent(data, ""))
+        assertEquals(expected, Calculator.filterByContent(data, null))
+    }
+
+    @Test
+    fun shouldThrow() {
+        val data = Collections.emptyList<Data>()
+
+        assertThrows(IllegalArgumentException::class.java) {
+            Calculator.filterByContent(data, "\\")
+        }
+    }
+
 }
